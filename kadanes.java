3df032fa3480d95a80cfd03e760103b65b70e86b
Atanu Debnath <atanuc2005@gmail.com>
2025-10-31 18:20:28 +0530

    add(kadanes.java): add a program to find maximum sum of subarrays using kadanes algorithm

---
+++ kadanes.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class kadanes {
+    //calculate maximum sum using kadanes algorithm
+    //key point: if(crr < 0) {crr = 0}
+    public static int kadanes(int[] arr) {
+        int crr = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            crr += arr[i];
+            if(crr < 0) crr = 0;
+            max = Math.max(crr, max);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        System.out.println("Enter the value of the array: ");
+        for(int i=0; i<num; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(kadanes(arr));
+        sc.close();
+    }
+}
